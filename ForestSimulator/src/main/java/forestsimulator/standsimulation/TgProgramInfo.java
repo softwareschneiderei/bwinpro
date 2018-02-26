99cbe1327d73f2916cd06c36b60dc84188f4565f
Mihael Koep <mihael.koep@softwareschneiderei.de>
2018-02-26 17:23:56 +0100

    BWIN-15: Add a slightly adjusted Logo for ForstBW.

--- ForestSimulator/src/main/java/forestsimulator/standsimulation/TgProgramInfo.java
+++ ForestSimulator/src/main/java/forestsimulator/standsimulation/TgProgramInfo.java
@@ -1,8 +1,8 @@
-/* http://www.nw-fva.de
+/** http://www.nw-fva.de
    Version 07-11-2008
 
    (c) 2002 Juergen Nagel, Northwest German Forest Research Station, 
-       Grätzelstr.2, 37079 Göttingen, Germany
+       GrÃ¤tzelstr.2, 37079 GÃ¶ttingen, Germany
        E-Mail: Juergen.Nagel@nw-fva.de
  
 This program is free software; you can redistribute it and/or
@@ -15,20 +15,21 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
  */
 package forestsimulator.standsimulation;
+
 import javax.swing.*;
+
 /**
  *
- * @author  J. Nagel
+ * @author J. Nagel
  */
 public class TgProgramInfo extends JPanel {
-    
-    /** Creates a new instance of TgProgramInfo */
+
     public TgProgramInfo(TgJFrame parent) {
-            String name = parent.plugIn;
-            name = name.replaceAll(".xml", ".jpg");
-	    ImageIcon startImage = new ImageIcon(parent.programDir+System.getProperty("file.separator")+"models"+System.getProperty("file.separator")+
-                    "logo"+System.getProperty("file.separator")+name);
-            JLabel jLabelBild = new JLabel(startImage);
-            add(jLabelBild);
- 
-}}
+        String name = parent.plugIn;
+        name = name.replaceAll(".xml", ".jpg");
+        ImageIcon startImage = new ImageIcon(parent.programDir + System.getProperty("file.separator") + "models" + System.getProperty("file.separator")
+                + "logo" + System.getProperty("file.separator") + name);
+        JLabel jLabelBild = new JLabel(startImage);
+        add(jLabelBild);
+    }
+}
