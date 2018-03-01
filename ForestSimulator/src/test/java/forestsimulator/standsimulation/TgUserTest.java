e36e4b41151b9b832bb04e527a30db7d4f0e8217
Mihael Koep <mihael.koep@softwareschneiderei.de>
2018-03-01 12:00:37 +0100

    BWIN-18: Fix path sensitive test for linux.

--- ForestSimulator/src/test/java/forestsimulator/standsimulation/TgUserTest.java
+++ ForestSimulator/src/test/java/forestsimulator/standsimulation/TgUserTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TgUserTest {
-    private static final File WORKING_DIRECTORY = new File("D:/installation/location");
+    private static final File WORKING_DIRECTORY = new File("D:\\some\\base\\directory");
     private static final String INI_FILE = ".\\user\n"
             + ".\\data_standsimulation\n"
             + ".\\output_standsimulation\n"
@@ -21,13 +21,17 @@ public class TgUserTest {
     public void settingsParsedCorrectly() throws IOException {
         TgUser userSettings = new TgUser();
         userSettings.loadSettings(iniContent(), WORKING_DIRECTORY);
-        assertThat(userSettings.getProgramDir(), is(new File(WORKING_DIRECTORY, "user")));
-        assertThat(userSettings.getDataDir(), is(new File(WORKING_DIRECTORY, "data_standsimulation")));
-        assertThat(userSettings.getWorkingDir(), is(new File(WORKING_DIRECTORY, "output_standsimulation")));
+        assertThat(userSettings.getProgramDir(), is(canonicalFileOf(WORKING_DIRECTORY, "user")));
+        assertThat(userSettings.getDataDir(), is(canonicalFileOf(WORKING_DIRECTORY, "data_standsimulation")));
+        assertThat(userSettings.getWorkingDir(), is(canonicalFileOf(WORKING_DIRECTORY, "output_standsimulation")));
         assertThat(userSettings.getLanguageShort(), is("de"));
         assertThat(userSettings.getXMLSettings(), is("ForestSimulatorSettingsBW.xml"));
         assertThat(userSettings.getPlugIn(), is("ForestSimulatorSettingsBW.xml"));
         assertThat(userSettings.getGrafik3D(), is(0));
+    }
+
+    private static File canonicalFileOf(File base, String subdirectory) throws IOException {
+        return new File(base, subdirectory).getCanonicalFile();
     }
 
     @Test
