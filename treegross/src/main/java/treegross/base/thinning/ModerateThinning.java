6418d6786d01c4dc1f523eb5dd27045eef4c01ef
Mihael Koep <mihael.koep@softwareschneiderei.de>
2018-07-06 10:06:45 +0200

    BWIN-52: Reviewed moderate thinning factor selection with domain experts and documented the results.

--- treegross/src/main/java/treegross/base/thinning/ModerateThinning.java
+++ treegross/src/main/java/treegross/base/thinning/ModerateThinning.java
@@ -7,6 +7,13 @@ import treegross.base.Tree;
 public interface ModerateThinning {
     static final double defaultThinningFactor = 1d;
     
+    /**
+     * A norm tree is used to select the thinning factor based on certain attributes
+     * like height or age. Usually they are based on the tree's species.
+     * 
+     * @param tree norm tree for a species
+     * @return thinning factor for certain attributes of the norm tree
+     */
     double thinningFactorFor(Tree tree);
     
     boolean shouldReduce(Species species);
