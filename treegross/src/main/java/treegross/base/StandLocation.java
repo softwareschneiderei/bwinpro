1a5e8e4f44f76bc7bf1cd2877d872d746bf61c23
Mihael Koep <mihael.koep@softwareschneiderei.de>
2018-08-01 16:37:45 +0200

    Add new constructor for StandLocation taking 3 arguments matching the climate database.

--- treegross/src/main/java/treegross/base/StandLocation.java
+++ treegross/src/main/java/treegross/base/StandLocation.java
@@ -3,10 +3,20 @@ package treegross.base;
 public class StandLocation {
 
     public final String federalState;
-    public final String subRegion;
+    public final String growingSubRegion;
+    public final String growingRegion;
 
-    public StandLocation(String federalState, String subregion) {
+    public StandLocation(String federalState, String growingSubRegion) {
+        this(federalState, regionFrom(growingSubRegion), growingSubRegion);
+    }
+    
+    public StandLocation(String federalState, String growingRegion, String growingSubRegion) {
         this.federalState = federalState;
-        this.subRegion = subregion;
+        this.growingRegion = growingRegion;
+        this.growingSubRegion = growingSubRegion;
+    }
+    
+    private static String regionFrom(String growthSubregion) {
+        return growthSubregion.split("/")[0];
     }
 }
