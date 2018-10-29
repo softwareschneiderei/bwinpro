2b3e03c139999e25aeb2e007646b912f85053a9b
Mihael Koep <mihael.koep@softwareschneiderei.de>
2018-10-29 12:03:25 +0100

    BWIN-97: Add missing changes.

--- treegross/src/main/java/treegross/dynamic/siteindex/EnvironmentNormalizer.java
+++ treegross/src/main/java/treegross/dynamic/siteindex/EnvironmentNormalizer.java
@@ -1,15 +1,31 @@
 package treegross.dynamic.siteindex;
 
-public class EnvironmentStandardizer {
+public class EnvironmentNormalizer {
 
-    private EnvironmentStandardizer() {
+    private EnvironmentNormalizer() {
         super();
     }
     
-    public static EnvironmentVariables standardize(EnvironmentVariables variables) {
-        EnvironmentVariables mean5YearVariables = variables.calculate5YearMeans();
-        LongtermEnvironmentVariables longTermVariables = new LongtermEnvironmentVariables(mean5YearVariables);
+    public static EnvironmentVariables normalize(EnvironmentVariables variables) {
+        LongtermEnvironmentVariables longtermVariables = new LongtermEnvironmentVariables(variables);
         
-        return longTermVariables.standardized(variables.calculateWeighted5YearMeans());
+        return standardized(longtermVariables, variables.calculateWeighted5YearMeans());
+    }
+    
+    private static EnvironmentVariables standardized(LongtermEnvironmentVariables longtermVariables, EnvironmentVariables environment) {
+        EnvironmentVariables result = new EnvironmentVariables();
+        for (SeasonMeanValues yearlyValues : environment) {
+            result.addGrowingSeason(new SeasonMeanValues(
+                    yearlyValues.year,
+                    standardizeValues(yearlyValues.meanTemperature, longtermVariables.growingSeasonMeanTemperature()),
+                    standardizeValues(yearlyValues.meanPrecipitationSum, longtermVariables.growingSeasonPrecipitationSum()),
+                    yearlyValues.aridityIndex,
+                    new AnnualNitrogenDeposition(standardizeValues(yearlyValues.nitrogenDeposition.value, longtermVariables.nitrogenDeposition().value))));
+        }
+        return result;
+    }
+
+    private static double standardizeValues(double yearlyMean, double longtermMean) {
+        return (yearlyMean - longtermMean) / longtermMean;
     }
 }
